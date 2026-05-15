373e1a0ac17e218ca8f5decb2341a57f97f9ddc1
Kalab Goitom <kalabgoitommerese@gmail.com>
2026-05-15 14:35:31 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const path = require("path");
-console.log(path.sep);
+const { readFileSync, writeFileSync, write } = require("fs");
 
-const filePath = path.join("/content", "subfolder", "test.txt");
-console.log(filePath);
+const first = readFileSync("./content/first.txt", "utf-8");
+const second = readFileSync("./content/second.txt", "utf-8");
 
-const base = path.basename(filePath);
-console.log(base);
-
-const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
-console.log(absolute);
+// console.log(first, second);
+writeFileSync(
+  "./content/result-sync.txt",
+  `Here is the result: ${first}, ${second}`,
+  {
+    flag: "a",
+  },
+);
